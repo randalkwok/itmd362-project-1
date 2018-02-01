30cb3cb6680901d1e71a736b5a2772b3c59ccba4
Randal <rkwok1@hawk.iit.edu>
2018-02-01 14:26:51 -0600

    Add basic form data check

---
+++ js/site.js
@@ -0,0 +1,45 @@
+$('#signup-form').on('submit',
+  function(e) {
+    var phone = $('#phone').val();
+    var re = /\d{3}.*\d{3}.*\d{4}/;
+    var email = $('#email').val();
+    var re2 = /.*@.*/;
+    var name = $('#name').val();
+    var validPhone = false;
+    var validEmail = false;
+    var validName = false;
+    e.preventDefault();
+    if(re.test(phone))
+    {
+      validPhone = true;
+      $('#phone-warning').remove();
+    }
+    else{
+      if(!$('#phone-warning').length) {
+        $('#phone-input').append('<li id="phone-warning">Please enter a proper phone number</li>');
+      }
+    }
+    if(re2.test(email))
+    {
+      validEmail = true;
+      $('#email-warning').remove();
+    }
+    else{
+      if(!$('#email-warning').length){
+        $('#email-input').append('<li id="email-warning">Please enter a proper e-mail address</li>');
+      }
+    }
+    if(name.length > 0){
+      validName = true;
+    }
+    else{
+      if(!$('#name-warning').length){
+        $('#name-input').append('<li id="name-warning">Please enter a name</li>');
+      }
+    }
+    if(validPhone && validEmail && validName){
+      $(this).remove();
+      $('body').append('<h1>Thank You For Signing Up</h1>');
+    }
+  }
+);
